40157b49f9e6929f9200b7def8b9542ebc366de1
fjri039rmfir948 <katya.kostina.2025@inbox.ru>
2026-06-24 11:29:18 +0300

    Обновить main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    music.play(music.stringPlayable("C5 C5 G G G G G C5 ", 120), music.PlaybackMode.UntilDone)
+})
 basic.showString("Hello!")
 for (let index = 0; index < 4; index++) {
+    music.play(music.stringPlayable("B C5 B B C C E C ", 120), music.PlaybackMode.UntilDone)
     basic.showLeds(`
         # . # . #
         # . # . #
@@ -8,6 +12,4 @@ for (let index = 0; index < 4; index++) {
         # # # # #
         `)
 }
-basic.forever(function () {
-    music.play(music.stringPlayable("B C5 B B C C E C ", 120), music.PlaybackMode.UntilDone)
-})
+basic.showString("Godbay")
